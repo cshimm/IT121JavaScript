5798d7634bf392599bd7a50740131a524deadf84
cshimm <dameepman@gmail.com>
2023-01-07 20:07:03 +0000

    Added week 1 directory and  HW 1

---
+++ Week1/script.js
@@ -0,0 +1,6 @@
+const firstName = "Cameron";
+const lastName = "Shimmin";
+let yearsOfStudy = 1.5;
+let goal = "pursue a Bachelors degree in computer science";
+
+document.write(`Hello my name is ${firstName} ${lastName}. I have been in SCCC's Software Engineering AAS-T for ${yearsOfStudy} years. I plan to ${goal} after completing the AAS-T.`)
