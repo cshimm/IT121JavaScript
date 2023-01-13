73ab58a48b3ab251416104c093cb2952cb481e91
cshimm <dameepman@gmail.com>
2023-01-13 23:18:05 +0000

    polish for week 2 assignment

--- Week2/script.js
+++ Week2/script.js
@@ -2,19 +2,19 @@
 // Cameron Shimmin
 // 1.13.23
 
-// Grab user input, force input to lower case
-let userInput = prompt("Please enter a word").toLowerCase();
+// Grab user input
+let userInput = prompt("Please enter a word");
 
 // Reprompt if user input is empty
 while (userInput.length === 0){
-  userInput = prompt("Input cannot be empty. Please enter a word").toLowerCase();
+  userInput = prompt("Input cannot be empty. Please enter a word");
 }
-// split userInput by spaces (" ")
-const splitInput = userInput.split(" ");
+// split userInput by spaces (" "), force input to lower case
+const splitInput = userInput.toLowerCase().split(" ");
 
 // define vowels to check against
 const vowels = "aeiouy";
-let pigLatinPhrase = "";
+let pigLatinTranslation = "";
 
 // loop through the words in userInput and check if they start with a vowel, consenant or two consenants
 splitInput.forEach(word => {
@@ -27,20 +27,19 @@ splitInput.forEach(word => {
   else if (!vowels.includes(word.substring(1, 2))) {
     //Starts with two consenants
     const firstTwoLetters = word.slice(0, 2);
-    let restOfWord = word.slice(2, word.length);
+    const restOfWord = word.slice(2, word.length);
 
     pigLatinWord = restOfWord + firstTwoLetters + "ay";
   }
   else {
     // Starts with one consenant
     const firstLetter = word.slice(0, 1);
-    let restOfWord = word.slice(1, word.length);
+    const restOfWord = word.slice(1, word.length);
 
     pigLatinWord = restOfWord + firstLetter + "ay";
   }
 
   // Append result of pig latin translation to pig latin phrase
-  pigLatinPhrase += (pigLatinWord + " ");
+  pigLatinTranslation += (pigLatinWord + " ");
 })
-alert(userInput);
-alert(pigLatinPhrase);
+alert(`\"${userInput}\" translated into pig latin is: \"${pigLatinTranslation}\"`);
